5d47685e72d19677f11ed284714d70eeaae75133
Hr1stoff <igorvajternko7@gmail.com>
2025-03-18 07:08:24 +0800

    update createUsersSQL file

--- DB/создание акканута для работы с интефейсом.sql
+++ DB/создание акканута для работы с интефейсом.sql
@@ -1,3 +1,30 @@
+INSERT INTO access_rights (department_id, position_id, system_id, user_type)
+VALUES 
+    ((SELECT id FROM departments WHERE name = 'ИТ ЛАБОРАТОРИЯ'),
+     (SELECT id FROM positions WHERE name = 'Техник-программист'),
+     8,
+     'ФИО')
+RETURNING id;
+
+INSERT INTO access_rights_attr (access_rights_id, system_attribute_id)
+VALUES (
+    (SELECT id FROM access_rights 
+     WHERE department_id = (SELECT id FROM departments WHERE name = 'ИТ ЛАБОРАТОРИЯ')
+     AND position_id = (SELECT id FROM positions WHERE name = 'Техник-программист')
+     AND system_id = 8
+     ORDER BY id DESC LIMIT 1),
+    1
+)
+RETURNING id;
+
+
+INSERT INTO system_attributes (system_id, name, value)
+VALUES 
+    (8, 'role', 'admin'),
+    (8, 'role', 'moderator'),
+    (8, 'role', 'user')
+RETURNING id;
+
 
 INSERT INTO users (encrypted_inn, full_name, mail, phone, department_id, position_id, active, tg_id, tg_status, created_at, updated_at)
 VALUES 
